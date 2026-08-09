dbbddeb9b1c9ae60b622fb4f641b8cda59ad0527
Giliard31 <113076201+Giliard31@users.noreply.github.com>
2026-08-09 12:47:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import webbrowser
 from datetime import datetime
 from flask import Flask, request, jsonify, render_template_string
 from flask_cors import CORS
-from iqoptionapi.stable_api import IQ_Option
 
 try:
     import pytz
